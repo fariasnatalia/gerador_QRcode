675f5c462a8439f038ed96591cf2b2811da1ee48
natalia Farias Santos <102639784+fariasnatalia@users.noreply.github.com>
2023-10-13 22:53:12 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { useState } from 'react'; 
+import QRCode from 'react-qr-code';
+import QRCodeLink from 'qrcode';
+
+import './App.css';
+
+
+function App() {
+  const [link, setLink] = useState('')
+  const [qrcodeLink, setQrcodeLink] = useState('')
+
+ 
+  function handleGenerate(link_url){
+    QRCodeLink.toDataURL(link_url,{
+      width: 600,
+      margin: 3,
+    }, function(err, url){
+      setQrcodeLink(url);
+     
+    })
+  }
+
+
+  function handleQrcode(e){
+    setLink(e.target.value);
+    handleGenerate(e.target.value)
+  }
+
+  return (
+    <div  className= "container" >
+    
+    <QRCode 
+      value = {link}
+    />
+
+    
+        <input 
+          className="input"
+          plceholder= "Digite o seu link..."
+          value={link}
+          onChange={  (e) => handleQrcode(e)}
+        />
+
+        <a href={qrcodeLink} download={`qrcode.png`} >Baixar QRCode</a>
+      
+    </div>
+  );
+}
+
+export default App;
